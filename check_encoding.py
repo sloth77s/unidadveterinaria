7518a8a0f2faae359012f96a8b8cb7a9f4a4ca57
sloth77s <nicolasaguilarliscano501@gmail.com>
2026-09-21 22:52:48 -0500

    chore: move site files to root for Cloudflare Pages deploy

--- check_encoding.py
+++
@@ -1,7 +0,0 @@
-import os
-for fname in ['aviso-legal.html', 'politica-de-privacidad.html', 'politica-de-cookies.html']:
-    path = f'unidad-veterinaria-david-aguilar/{fname}'
-    with open(path, 'rb') as f:
-        raw = f.read(100)
-        has_bom = raw.startswith(b'\xef\xbb\xbf')
-        print(f'{fname}: has_bom={has_bom}, starts={raw[:20]}')
